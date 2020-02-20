ee9be5d822edf3d2f732faf382a0db7e52f3ce8c
Norkkom <CATBIRDS@users.noreply.github.com>
2020-02-20 17:11:58 -0500

    Update Surveillance.py
    Added new optional argument "schizo" which will output the original, fragmented generation. By default now running the `speak command without this argument will generate longer and more coherent output.

--- src/Surveillance.py
+++ src/Surveillance.py
@@ -3,6 +3,7 @@ import json
 import os
 import numpy as np
 import tensorflow as tf
+import re
 
 import model, sample, encoder
 
@@ -18,7 +19,7 @@ def generate(prompt):
     seed = None
     nsamples = 1
     batch_size = 1
-    length = 120
+    length = None
     temperature = 0.7
     top_k = 40
     top_p = 1
@@ -33,10 +34,15 @@ def generate(prompt):
     with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
         hparams.override_from_dict(json.load(f))
 
-    with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
+    if length is None:
+        length = hparams.n_ctx // 2
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
-        tf.set_random_seed(seed)
+        tf.compat.v1.set_random_seed(seed)
         output = sample.sample_sequence(
             hparams=hparams, length=length,
             context=context,
@@ -44,7 +50,7 @@ def generate(prompt):
             temperature=temperature, top_k=top_k, top_p=top_p
         )
 
-        saver = tf.train.Saver()
+        saver = tf.compat.v1.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
@@ -62,15 +68,26 @@ bot = commands.Bot(command_prefix='`')
 bot.remove_command("help")
 
 @bot.command()
-async def speak(ctx, arg):
+async def speak(ctx, arg1, arg2=''):
     async with ctx.channel.typing():
-        await ctx.send(generate(arg))
+        if (arg1 == "schizo" and arg2 != ''):
+            message = str(generate(arg2))
+            message = re.sub('(<\|endoftext\|>)', '', message) # Regex to remove this commonly occurring garbage
+            message = message[:500]
+        else:
+            message = generate(arg1)
+            message = re.sub('(<\|endoftext\|>)', ' ', message) # Regex to remove this commonly occurring garbage
+            message = message[:1000]
+            message = re.sub('(?<=\.)[^.]*\Z', '', message) # Regex to remove anything after the last period, if applicable
+
+        await ctx.send(message)
 
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title="Commands", description="All commands use the ` prefix")
     embed.add_field(name="`help", value="Shows this message!", inline=False)
     embed.add_field(name="`speak \"[prompt]\"", value="Generates GPT-2 output from the given prompt.", inline=False)
+    embed.add_field(name="`speak schizo \"[prompt]\"", value="Generates fragmented and sometimes incoherent GPT-2 output from the given prompt.", inline=False)
     await ctx.send(embed=embed)
 
 bot.run('YOUR_TOKEN_HERE')
